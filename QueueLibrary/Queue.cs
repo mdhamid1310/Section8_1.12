5c9ec8f714656640b0ea31bbc772a2b6ccf20639
mdhamid1310 <john@example.com>
2023-10-27 09:21:06 +0530

    Add project files.

---
+++ QueueLibrary/Queue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueLibrary
+{
+    public class Queue
+    {
+        private List<int> items = new List<int>();
+
+        public void Enqueue(int data)
+        {
+            items.Add(data);
+        }
+
+        public int Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            int data = items[0];
+            items.RemoveAt(0);
+            return data;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            return items[0];
+        }
+
+        public bool IsEmpty()
+        {
+            return items.Count == 0;
+        }
+    }
+
+}
---
+++ Section8_1.12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QueueLibrary;
+
+namespace Section8_1._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+            Console.ReadLine();
+        }
+
+        private static void runApp()
+        {
+            Queue queue = new Queue();
+
+            // Enqueue elements into the queue
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            Console.WriteLine("Queue elements:");
+            while (!queue.IsEmpty())
+            {
+                int element = queue.Dequeue();
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
